2fe0a1d4c11860a332c8514b85067f786445f3bf
cessnaxls <cessnaxls>
2026-09-16 00:11:57 -0400

    U

--- build.mjs
+++ build.mjs
@@ -1,7 +1,11 @@
 import fs from 'fs';
 fs.rmSync('dist',{recursive:true,force:true});
 fs.mkdirSync('dist/vendor',{recursive:true});
-fs.copyFileSync('index.html','dist/index.html');
+let html=fs.readFileSync('index.html','utf8')
+ .replaceAll('/node_modules/leaflet/dist/leaflet.css','vendor/leaflet.css')
+ .replaceAll('/node_modules/leaflet/dist/leaflet.js','vendor/leaflet.js')
+ .replaceAll('/node_modules/jszip/dist/jszip.min.js','vendor/jszip.min.js');
+fs.writeFileSync('dist/index.html',html);
 fs.cpSync('public','dist',{recursive:true});
 fs.copyFileSync('node_modules/leaflet/dist/leaflet.css','dist/vendor/leaflet.css');
 fs.copyFileSync('node_modules/leaflet/dist/leaflet.js','dist/vendor/leaflet.js');
